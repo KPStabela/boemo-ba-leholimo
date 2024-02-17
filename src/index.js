e87fb9eedffbe373c259a8167adde61dde1d471a
KP-Stabela <159870825+KP-Stabela@users.noreply.github.com>
2024-02-17 03:14:32 +0200

    Added search functionality to JS using axios and api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
-function searchCity(event) {
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+    let apiKey = `247b1tffdc3d60aa05b1o283fb0ad4ee`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&metric`;
+    
+    axios.get(apiUrl).then(refreshWeather);
+}
+
+
+function updateCity(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+
+    searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", searchCity);
+searchFormElement.addEventListener("submit", updateCity);
 
+searchCity("Klerksdorp");
