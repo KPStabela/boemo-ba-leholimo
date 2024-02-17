9c03d397adb612579503f509b35e59e8ad6cf494
KP-Stabela <159870825+KP-Stabela@users.noreply.github.com>
2024-02-17 03:59:38 +0200

    Added functionality that has detailed weather and current time and date

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-function refreshWeather(response) {
+function updateWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#detailed-condition");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let humidityElement = document.querySelector("#humidity");
+    let date = new Date(response.data.time * 1000);
 
     cityElement.innerHTML = response.data.city;
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    timeElement.innerHTML = formatDate(date);
     temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let day = days[date.getDay()];
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`
+    }
+
+    return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
     let apiKey = `247b1tffdc3d60aa05b1o283fb0ad4ee`;
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&metric`;
     
-    axios.get(apiUrl).then(refreshWeather);
+    axios.get(apiUrl).then(updateWeather);
 }
 
 
@@ -25,3 +48,4 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", updateCity);
 
 searchCity("Klerksdorp");
+
